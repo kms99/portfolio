0e7fba38e7bb4aa402f39d36c399547051fe5933
Kim MyungSub <audtjqxx@naver.com>
2024-03-09 05:14:29 +0900

    [style]: project란 마진 수정

--- app/components/sections/project/ProjectSection.tsx
+++ app/components/sections/project/ProjectSection.tsx
@@ -5,7 +5,7 @@ import { PROJECT_LISTS } from './constants';
 
 export default function ProjectSection() {
   return (
-    <section className="h-screen flex flex-col items-center relative" id="project">
+    <section className="min-h-screen h-auto flex flex-col items-center relative" id="project">
       <SubTitle>프로젝트</SubTitle>
       <ul className="grid grid-cols-3 justify-items-center gap-8">
         {PROJECT_LISTS.map(project => (
