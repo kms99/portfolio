34485be9de820244649c881dca48fa042b9a0ffe
Kim MyungSub <audtjqxx@naver.com>
2024-03-13 16:09:36 +0900

    [style] 미디어 쿼리 적용 search box 크기조절

--- app/components/sections/intro/IntroSearchBox.tsx
+++ app/components/sections/intro/IntroSearchBox.tsx
@@ -9,17 +9,13 @@ interface Props {
 
 export default function IntroSearchBox({ checkViewPort }: Props) {
   return (
-    <div className="border-black border-4 desktop:w-12em tablet:w-10em w-7em text-5xl py-6 px-4 relative font-bold">
-      <h2
-        className={`font-bold w-7em desktop:text-5xl tablet:text-4xl text-3xl ${
-          checkViewPort && 'animate-typing'
-        } whitespace-nowrap overflow-hidden border-r border-black`}
-      >
+    <div className="relative font-bold border-4 border-black searchBoxMobile tablet:searchBoxTablet desktop:searchBoxDesktop">
+      <h2 className={`w-7em whitespace-nowrap border-r border-black ${checkViewPort && 'animate-typing'}`}>
         {ONE_LINE_NAME}
       </h2>
       <a
         onClick={e => scrollToElement(e, 'about')}
-        className="absolute right-8 top-1/2 -translate-y-1/2"
+        className="absolute right-4 scale-50 tablet:scale-100 tablet:right-8  top-1/2 -translate-y-1/2"
         href="#about"
         aria-label="소개 섹션으로 이동"
       >
