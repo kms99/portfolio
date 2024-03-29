cb892ddeb37cb24c0a538e652247b8606f6ac45b
Kim MyungSub <audtjqxx@naver.com>
2024-03-30 00:03:37 +0900

    [style]: book second page 반응형 추가

--- app/components/sections/about/secondPage/SecondPageBack.tsx
+++ app/components/sections/about/secondPage/SecondPageBack.tsx
@@ -10,11 +10,10 @@ export default function SecondPageBack() {
     <AboutPageBack countTitle="두번째장" title="나의 정보" detail="기본 정보 알아봅니다.">
       <Image
         src={profile}
-        height={250}
         alt="user default"
-        className="absolute left-1/2 bottom-28 -translate-x-1/2 rotate-3 shadow-ProfileImageShadow z-10"
+        className="absolute left-1/2 bottom-16 -translate-x-1/2 rotate-3 shadow-ProfileImageShadow z-10 h-[15em] w-auto"
       />
-      <span className="-rotate-45 absolute -right-12 bottom-24 text-6xl text-gray-400">MY INFO</span>
+      <span className="-rotate-45 absolute -right-12 bottom-20 text-[3em] text-gray-400">MY INFO</span>
 
       <PageNav mode={BookMode.PREV} />
     </AboutPageBack>
